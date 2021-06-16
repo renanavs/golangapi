0f4532a77c04b911ba8ecf4049e8049b2ba4b0ef
Renan Albuquerque <renanavs@pop-os.localdomain>
2021-06-15 21:56:42 -0300

    feature: user controller CRUD

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -19,6 +19,10 @@ const MSG_MISSING_ID = "Id must be defined"
 const MSG_ID_INTEGER = "Id must be numeric"
 const MSG_CREATE_USER = "Cannot create user"
 const MSG_LIST_USER = "Cannot create user"
+const MSG_FIND_BY_ID = "Cannot find user"
+const MSG_UPDATE_ERROR = "Cannot update user"
+const MSG_DELETE_ERROR = "Cannot delete user"
+const MSG_DELETED = "User Deleted"
 
 func (controller UserController) List(c *gin.Context) {
 	db, err := database.GetDatabase()
@@ -92,10 +96,24 @@ func (controller UserController) Read(c *gin.Context) {
 		log.Println(err.Error())
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"user":   id,
-		"status": "read",
-	})
+	db, err := database.GetDatabase()
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": MSG_DATABASE_CONNECTION,
+		})
+		return
+	}
+	var user model.User
+	err = db.First(&user, id).Error
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": MSG_FIND_BY_ID,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
 
 func (controller UserController) Update(c *gin.Context) {
@@ -106,11 +124,51 @@ func (controller UserController) Update(c *gin.Context) {
 		})
 		return
 	}
-	id, _ := strconv.Atoi(user_id)
-	c.JSON(http.StatusOK, gin.H{
-		"user":   id,
-		"status": "update",
+	db, err := database.GetDatabase()
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": MSG_DATABASE_CONNECTION,
+		})
+		return
+	}
+	var user model.User
+	err = c.ShouldBindJSON(&user)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": MSG_JSON_BIND,
+		})
+		return
+	}
+	result := db.Where("id = ?", user_id).Updates(model.User{
+		Name:  user.Name,
+		Login: user.Login,
+		Email: user.Email,
 	})
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "Rows affected: 0",
+		})
+		return
+	}
+	err = result.Error
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": MSG_UPDATE_ERROR,
+		})
+		return
+	}
+	err = db.First(&user, user.ID).Error
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": MSG_FIND_BY_ID,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, user)
 }
 
 func (controller UserController) Delete(c *gin.Context) {
@@ -121,9 +179,38 @@ func (controller UserController) Delete(c *gin.Context) {
 		})
 		return
 	}
-	id, _ := strconv.Atoi(user_id)
+	id, err := strconv.Atoi(user_id)
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": MSG_ID_INTEGER,
+		})
+		return
+	}
+	db, err := database.GetDatabase()
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": MSG_DATABASE_CONNECTION,
+		})
+		return
+	}
+	result := db.Delete(&model.User{}, id)
+	err = result.Error
+	if err != nil {
+		log.Println(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"msg": MSG_FIND_BY_ID,
+		})
+		return
+	}
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"msg": "Rows affected: 0",
+		})
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{
-		"user":   id,
-		"status": "delete",
+		"msg": MSG_DELETED,
 	})
 }
